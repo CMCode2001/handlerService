e6b07ea0491e310f77e351be98fc71068e2168d4
CMCode2001 <cheikh.mb2022@gmail.com>
2023-11-07 17:54:22 +0000

    First Push

---
+++ handlerService/src/main/java/com/gestionEvent/handlerService/HandlerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gestionEvent.handlerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlerServiceApplication.class, args);
+	}
+
+}
